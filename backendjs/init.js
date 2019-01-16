0d352942c10363a13441b81ac170844abeef1144
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-01-16 09:57:07 +0530

    Removed top bar

---
+++ backendjs/init.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBccjlbh3IpQK_Lfc1mRPdxe1oFoY6_V3c",
+    authDomain: "esolved-2019.firebaseapp.com",
+    databaseURL: "https://esolved-2019.firebaseio.com",
+    projectId: "esolved-2019",
+    storageBucket: "esolved-2019.appspot.com",
+    messagingSenderId: "164798168950"
+};
+firebase.initializeApp(config);
